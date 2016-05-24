26c3a4183dc08caf99191ad75621234e23b95836
Gustavo Quiros <luis.quiros@prodigious.com>
2016-05-24 11:04:55 -0600

    Markup added to other pages

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -22,112 +22,6 @@ MyApp.angular.controller('IndexPageController', ['$scope', '$http', 'InitService
   });
 
 }]); 
-MyApp.angular
-.directive('footerDirective', function() {
-  var controller = ['$scope', function($scope){
-  	$scope.social = [
-        {href: "#", icon: "fa-facebook"},
-        {href: "#", icon: "fa-twitter"},
-        {href: "#", icon: "fa-youtube"},
-        {href: "#", icon: "fa-linkedin"},
-        {href: "#", icon: "fa-pinterest"}
-    ];
-
-    $scope.footernav = [
-      {text: "Home", href: "index.html"},
-      {text: "My Account", href: "account.html"},
-      {text: "Wishlist", href: "Wishlist.html"},
-      {text: "Cart", href: "cart.html"},
-      {text: "Order History", href: "#"}      
-    ];
-
-    $scope.categories = [
-      {text: "Mobile Phone", href: "#"},
-      {text: "Home Accessories", href: "#"},
-      {text: "LED TV", href: "#"},
-      {text: "Computer", href: "#"},
-      {text: "Gadgets", href: "#"}   
-    ];
-
-  }]
-
-  return {
-    restrict: 'E',
-    controller: controller,
-    templateUrl: 'partials/footer.html'
-  };
-});
-MyApp.angular
-.directive('headerDirective', function() {
-  var controller = ['$scope', '$location', 'dataService', function($scope, $location, dataService){
-  	$scope.login = true;
-  	$scope.usernav = [
-  		{text: "My Account", href: "account.html", icon: "fa-user"},
-  		{text: "Wishlist", href: "wishlist.html", icon: "fa-gift"},
-  		{text: "My Cart", href: "cart.html", icon: "fa-shopping-cart"}
-  	];
-
-  	
-
-  	$scope.currencies = [
-  		{currency: "USD"},
-  		{currency: "EUR"},
-  		{currency: "COL"}
-  	];
-
-  	dataService.getCurrency(function(argument) {
-  		console.log(argument, "Currency");
-  	}, function() {
-  		alert("Error");
-  	});
-
-  	$scope.selectedCurrency = $scope.currencies[0].currency;
-    $scope.currencySelection = function (item) {
-        $scope.selectedCurrency = item;
-    };
-
-	
-	dataService.getLanguage(function(argument) {
-  		console.log(argument, "Language");
-  	}, function() {
-  		alert("Error");
-  	});    
-
-    $scope.languages = [
-  		{language: "English"},
-  		{language: "French"},
-  		{language: "Spanish"}
-  	];
-  	$scope.selectedLanguage = $scope.languages[0].language;
-    $scope.languageSelection = function (lang) {
-        $scope.selectedLanguage = lang;
-    };
-
-    
-
-    $scope.menu = [
-  		{text: "Home", href: "index.html"},
-  		{text: "Shop Page", href: "shop.html"},
-  		{text: "Single Product", href: "single-product.html"},
-  		{text: "Cart", href: "cart.html"},
-  		{text: "Checkout", href: "checkout.html"},
-  	];
-	
-	$scope.isActive = function (viewLocation) { 
-     	if (viewLocation == window.location.pathname.split('/')[1]) {
-     		return 'active';
-     	}
-    };
-
-  }]
-
-  return {
-    restrict: 'E',
-    controller: controller,
-    templateUrl: 'partials/header.html'
-  };
-});
-console.log('Angular Directive');    
 MyApp.angular.factory('InitService', ['$document', function ($document) {
   'use strict';
 
@@ -190,3 +84,112 @@ MyApp.angular.factory('dataService', ['$document', '$http', function ($document,
 
   return pub;
 }]);
+
+MyApp.angular
+.directive('footerDirective', function() {
+  var controller = ['$scope', function($scope){
+  	$scope.social = [
+        {href: "#", icon: "fa-facebook"},
+        {href: "#", icon: "fa-twitter"},
+        {href: "#", icon: "fa-youtube"},
+        {href: "#", icon: "fa-linkedin"},
+        {href: "#", icon: "fa-pinterest"}
+    ];
+
+    $scope.footernav = [
+      {text: "Home", href: "index.html"},
+      {text: "My Account", href: "account.html"},
+      {text: "Wishlist", href: "Wishlist.html"},
+      {text: "Cart", href: "cart.html"},
+      {text: "Order History", href: "#"}      
+    ];
+
+    $scope.categories = [
+      {text: "Mobile Phone", href: "#"},
+      {text: "Home Accessories", href: "#"},
+      {text: "LED TV", href: "#"},
+      {text: "Computer", href: "#"},
+      {text: "Gadgets", href: "#"}   
+    ];
+
+  }]
+
+  return {
+    restrict: 'E',
+    controller: controller,
+    templateUrl: 'partials/footer.html'
+  };
+});
+MyApp.angular
+.directive('headerDirective', function() {
+  var controller = ['$scope', '$location', 'dataService', function($scope, $location, dataService){
+  	$scope.login = true;
+  	$scope.usernav = [
+  		{text: "My Account", href: "account.html", icon: "fa-user"},
+  		{text: "Wishlist", href: "wishlist.html", icon: "fa-gift"},
+  		{text: "My Cart", href: "cart.html", icon: "fa-shopping-cart"}
+  	];
+
+  	
+
+  	$scope.currencies = [
+  		{currency: "USD"},
+  		{currency: "EUR"},
+  		{currency: "COL"}
+  	];
+
+  	dataService.getCurrency(function(argument) {
+  		$scope.title = argument.data.title;
+  		console.log($scope.title, "Currency");
+  	}, function() {
+  		alert("Error");
+  	});
+
+  	$scope.selectedCurrency = $scope.currencies[0].currency;
+    $scope.currencySelection = function (item) {
+        $scope.selectedCurrency = item;
+    };
+
+	
+	dataService.getLanguage(function(argument) {
+  		$scope.title = argument.data.title;
+  		console.log($scope.title, "Language");
+  	}, function() {
+  		alert("Error");
+  	});    
+
+    $scope.languages = [
+  		{language: "English"},
+  		{language: "French"},
+  		{language: "Spanish"}
+  	];
+  	$scope.selectedLanguage = $scope.languages[0].language;
+    $scope.languageSelection = function (lang) {
+        $scope.selectedLanguage = lang;
+    };
+
+    
+
+    $scope.menu = [
+  		{text: "Home", href: "index.html"},
+  		{text: "Shop Page", href: "shop.html"},
+  		{text: "Single Product", href: "single-product.html"},
+  		{text: "Cart", href: "cart.html"},
+  		{text: "Checkout", href: "checkout.html"},
+  	];
+	
+	$scope.isActive = function (viewLocation) { 
+     	if (viewLocation == window.location.pathname.split('/')[1]) {
+     		return 'active';
+     	}
+    };
+
+  }]
+
+  return {
+    restrict: 'E',
+    controller: controller,
+    templateUrl: 'partials/header.html'
+  };
+});
+console.log('Angular Directive');    
